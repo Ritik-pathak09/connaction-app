d169a766aa3545ecfc92d379626d9d5aabf17b44
Ritik-pathak09 <ritikpathak8570@gmail.com>
2026-08-11 14:45:32 +0530

    Add files via upload

---
+++ server.js
@@ -0,0 +1,27 @@
+const express = require('express');
+const http = require('http');
+const { Server } = require('socket.io');
+const path = require('path');
+
+const app = express();
+const server = http.createServer(app);
+const io = new Server(server);
+
+app.use(express.static(path.join(__dirname, 'public')));
+
+io.on('connection', (socket) => {
+    console.log('Ek user connect ho gaya:', socket.id);
+
+    socket.on('chat message', (data) => {
+        io.emit('chat message', data);
+    });
+
+    socket.on('disconnect', () => {
+        console.log('User disconnect ho gaya');
+    });
+});
+
+const PORT = process.env.PORT || 3000;
+server.listen(PORT, () => {
+    console.log(`Server port ${PORT} par chal raha hai`);
+});
